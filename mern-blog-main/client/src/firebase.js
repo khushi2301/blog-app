1d7c478cbb00cc1b774363e15836b32cdd62c76d
khushi2301 <garkhushi12@gmail.com>
2024-09-15 15:52:32 +0530

    first commit

---
+++ mern-blog-main/client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAf0-ATgYqWQKX79NBAEi4E8dwGyftn2B4",
+  authDomain: "blog-app-b69fc.firebaseapp.com",
+  projectId: "blog-app-b69fc",
+  storageBucket: "blog-app-b69fc.appspot.com",
+  messagingSenderId: "792033349786",
+  appId: "1:792033349786:web:dd4b8a2ee79bd560f4cf23"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
