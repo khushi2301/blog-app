b01f2f1650cdfa18a1c04f5f448bb73f24c97b6b
khushi2301 <garkhushi12@gmail.com>
2024-10-21 20:33:33 +0530

    new

--- mern-blog-main/client/src/components/CallToAction.jsx
+++ mern-blog-main/client/src/components/CallToAction.jsx
@@ -11,8 +11,8 @@ export default function CallToAction() {
                 Checkout these resources with 100 JavaScript Projects
             </p>
             <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
-                <a href="https://www.100jsprojects.com" target='_blank' rel='noopener noreferrer'>
-                    100 JavaScript Projects
+                <a href="https://www.w3schools.com/js/js_intro.asp" target='_blank' rel='noopener noreferrer'>
+                    Explore more about Javascript.
                 </a>
             </Button>
         </div>
